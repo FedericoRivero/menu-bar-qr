ab9eeeec53b4564712a16498d1205f36a0c7c236
Fede Rivero <federicorivero18@gmail.com>
2022-09-14 18:21:41 -0300

    menu blue 42 resto bar, react-pdf, lottiefiles-player.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.svg";
+import menuqr from "./assets/lottie/menu-qr.json";
+import errorpdf from "./assets/lottie/error.json";
+import { Player } from "@lottiefiles/react-lottie-player";
+import React, { useState } from "react";
+// import { Document, Page } from "react-pdf";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 function App() {
+  const file = "./pdf/carta_blue_septiembre_2022-comprimido.pdf";
+  const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="carta">
+      <header>
+        <img src={logo} alt="Blue-42-resto-bar-logo" />
+        <h1>Blue 42 resto bar</h1>
       </header>
+      <div className="carta__container">
+        <div className="carta__container__load">
+          <h2>Nuestro Menú de hoy : </h2>
+        </div>
+        <div className="carta__container__document">
+          <Document
+            loading={
+              <figure className="loading">
+                <Player
+                  autoplay
+                  loop
+                  background={"transparent"}
+                  src={menuqr}
+                  style={{ height: "250px", width: "250px" }}
+                ></Player>
+                <h3>Cargando Menú...</h3>
+              </figure>
+            }
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            error={
+              <figure className="error">
+                <Player
+                  autoplay
+                  loop
+                  background={"transparent"}
+                  src={errorpdf}
+                  style={{ height: "250px", width: "250px" }}
+                ></Player>
+                <h3>Compruebe su conexión a internet.</h3>
+              </figure>
+            }
+            // options={options}
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                renderMode="svg"
+                width={window.innerWidth}
+                scale={0.9}
+              />
+            ))}
+          </Document>
+        </div>
+      </div>
+      <footer>
+        <small>&copy; Blue 42 resto bar · 2022 · Federico Rivero</small>
+      </footer>
     </div>
   );
 }
